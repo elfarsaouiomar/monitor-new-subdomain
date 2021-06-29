d51a43273ff83913c02e075641da9e7bacddba0b
omar <adilfarsaoui7@gmail.com>
2021-06-30 00:12:52 +0100

    push config example

--- config.py
+++ config.py
@@ -1,13 +1,14 @@
 #slack webhook
-WHslack=""
+WHslack="https://hooks.slack.com/services/xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 
 # TelegrameBoot
-telegramToken=""
-chatId=
+telegramToken="xxxxxxxxxxxxxxxxxxxxxxxxxx"
+chatId="xxxxxxxxxxxxxxxx"
 
 # MongoDB DB
-dbHost=""
-dbPort=
+dbHost="127.0.0.1"
+dbPort=27017
 dbName=""
 dbUser=""
 dbPwd=""
+
