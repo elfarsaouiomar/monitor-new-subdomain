472d797698ce00159347060a08fa2c7a9d47158a
omarelfarsaoui <omar.elfarsaoui@hi5group.co.uk>
2021-07-13 09:16:40 +0100

    * fix deprecated function (dns resolve) :fire:

--- check-new-subdomain.py
+++ check-new-subdomain.py
@@ -1,125 +1,29 @@
 #!/usr/bin/env python
+# I don't believe in the license, you can do whatever you like
 
-version = "1.0"
+version = "1.1"
 
-import argparse, threading
-from requests import post, get
-from pymongo import MongoClient
-import requests.packages.urllib3
+
+import argparse
+import threading
 from json import loads
 import dns.resolver
-from datetime import datetime
-from termcolor import colored
+import requests.packages.urllib3
 from jinja2 import Template
-from config import dbPort, dbHost, chatId, WHslack, telegramToken
+from requests import get
+from termcolor import colored
+
+from src.ConfigDB import ConfigDB
+from src.Config import resolverList
+from src.Functions import getCurrentTime
+from src.Notifications import Notifications
 
 # disable requests warnings
 requests.packages.urllib3.disable_warnings()
 
-class Notify:
-
-    def viaTelegram(self, message):
-
-        """
-            send message via Telegram
-        """
-        try:
-            telegramUrl = "https://api.telegram.org/bot{0}/sendMessage".format(telegramToken)
-            req = post(telegramUrl, params={'text': message, 'chat_id': chatId, 'parse_mode': 'Markdown'}, headers={'Content-Type': 'application/json'})
-            if req.status_code != 200:
-                print(colored("[!] error wile sending Message \n[!] status code : {0}".format(req.status_code), "red"))
-
-            if req.status_code == 429:
-                print(colored("[!] Api Rate limit : ", "red"))
-
-        except KeyboardInterrupt:
-            print(colored("[!] Ctrl+c detected", "yellow"))
-            exit(0)
-
-        except Exception as e:
-            print(colored("[!] error while sending slack message \n [!] {}".format(e), "red"))
-
-    def viaSlack(self, message):
-
-        """
-            send message via slack
-        """
-        try:
-            req = post(WHslack, json={'text': ':new: {0}'.format(message)}, headers={'Content-Type': 'application/json'})
-            if req.status_code != 200:
-                print(colored("[!] error wile sending Message \n[!] status code : {0}".format(req.status_code), "red"))
-
-            if req.status_code == 429:
-                print(colored("[!] Api Rate limit : ", "red"))
-
-        except KeyboardInterrupt:
-            print(colored("[!] Ctrl+c detected", "yellow"))
-            exit(0)
-
-        except Exception as e:
-            print(colored("[!] error while sending slack message \n [!] {}".format(e), "red"))
-
-
-class ConnToDb:
-    """
-    Connection to mongodb
-    """
-    client = MongoClient(dbHost, dbPort, serverSelectionTimeoutMS=3000)
-    db = client['MonitoringSubdomain']
-    collection = db['subdomains']
-
-    def connect(self):
-        pass
-
-    def _findAll(self):
-        """
-        get all subdomain
-        :return:
-        """
-        return self.collection.find()
-
-    def _add(self, target):
-        """
-        take two arg
-        domian string
-        newsubdomian list of newSubdomian
-        """
-        self.collection.insert_one(target)
-
-    def _update(self, domain, newSubbdomain):
-        """
-        update document in DB
-        :param domain:
-        :return:
-        """
-        self.collection.update_one({"domain": domain}, {"$pushAll": {"subdomains": newSubbdomain}})
-
-    def _findOne(self, domain):
-        """
-        :param domain: take domain as params
-        :return: return Object of domain
-        """
-        return self.collection.find_one({"domain": domain})
-
-    def _delete(self, domain):
-        """
-        delete domain name from database
-        :param domain: domain name
-        :return:
-        """
-        self.collection.find_one_and_delete({"domain": domain})
-
-    def _close(self):
-        """
-         close  connection
-        :return:
-        """
-        self.client.close()
-
-
 class SubDomainMonitoring:
-    db = ConnToDb()
-    sendNotification = Notify()
+    db = ConfigDB()
+    sendNotification = Notifications()
     slack = telegram = saveToFile = False
 
     def parseCrtResponse(self, subdomains):
@@ -197,34 +101,31 @@ class SubDomainMonitoring:
             exit(0)
 
         except Exception as e:
-            print(colored("[!] error while requesing {} \n[!] {}".format(domain, e), "red"))
+            print(colored("[!] error while requesting {} \n[!] {}".format(domain, e), "red"))
 
         return resultSubdomains
 
     def scanSubdomain(self, subdomain):
-        """
-        Resolve subdomain
-        """
         dnsResolver = dns.resolver.Resolver()
-        dnsResolver.nameservers = ['1.1.1.1', '1.0.0.1', '8.8.8.8', '8.8.4.4', '9.9.9.9', '9.9.9.10','77.88.8.8', '77.88.8.1', '208.67.222.222', '208.67.220.220']
+        dnsResolver.nameservers = resolverList
 
         dnsResult = dict()
         dnsResult['subdomain'] = subdomain
         try:
             for qtype in ['A', 'CNAME']:
 
-                answers = dnsResolver.query(subdomain, qtype, raise_on_no_answer=False)
+                answers = dnsResolver.resolve(subdomain, qtype)
 
                 if answers.rrset is None:
                     pass
 
                 elif answers.rdtype == 1:
                     a_records = [str(i) for i in answers.rrset]
-                    dnsResult["A"] = a_records[0]
+                    dnsResult["A"] = a_records
 
                 elif answers.rdtype == 5:
                     cname_records = [str(i) for i in answers.rrset]
-                    dnsResult["CNAME"] = cname_records[0]
+                    dnsResult["CNAME"] = cname_records
 
         except KeyboardInterrupt:
             print(colored("[!] Ctrl+c detected", "yellow"))
@@ -234,7 +135,7 @@ class SubDomainMonitoring:
             pass
 
         except Exception as e:
-            print(colored("[!] Error while resolving subdomain \n[!] {}".format(e), "red"))
+            print(colored("[!!] Error while resolving subdomain \n[!] {}".format(e), "red"))
 
         finally:
             return dnsResult
@@ -245,10 +146,10 @@ class SubDomainMonitoring:
         :param message:
         """
         if self.slack:
-            self.sendNotification.viaSlack(message)
+            self.sendNotification.slack(message)
 
         if self.telegram:
-            self.sendNotification.viaTelegram(message)
+            self.sendNotification.telegrame(message)
 
     def compaire(self, subdomians):
         """
@@ -275,8 +176,6 @@ class SubDomainMonitoring:
                 if len(newsubDomain) > 0:
                     oldSubdomain = target.get('subdomains')
                     diff = [x for x in newsubDomain if x not in oldSubdomain]
-                    if self.saveToFile:
-                        self.saveResultIntoFile(newsubDomain)
 
                     if len(diff) > 0:
                         self.db._update(domain, diff)
@@ -300,22 +199,6 @@ class SubDomainMonitoring:
             tm = Template(template)
             msg = tm.render(subdomain=i.get('subdomain'), A=i.get('A'), cname=i.get('CNAME'))
             self.notify(msg)
-
-    def saveResultIntoFile(self, listNewSubdomain):
-        """
-        save the new subdomains to a file
-        """
-        try:
-            file = open("new-subdomain" + self.getCurrentTime() + ".txt", "w")
-            for subomain in listNewSubdomain:
-                file.write(subomain+"\n")
-
-        except KeyboardInterrupt:
-            print(colored("[!] Ctrl+c detected", "yellow"))
-            exit(0)
-
-        except Exception as e:
-            print(colored("[!] error while comparing result \n[!] {}".format(e), "red"))
 
     def add(self, domain):
         """
@@ -348,9 +231,6 @@ class SubDomainMonitoring:
         except Exception as e:
             print(colored("[!] {0}".format(error), "red"))
 
-    def getCurrentTime(self):
-        return datetime.now().strftime("%Y-%m-%d-%I-%M-%S")
-
     def listAllDomains(self):
         """
         list all domain monitoring in DB
@@ -374,14 +254,12 @@ class SubDomainMonitoring:
         print(colored("[+] delete {} from database".format(domain), "blue"))
 
     def export(self):
-        """
-        export all subdomain to text file
-        """
+        """ export all subdomain to text file """
 
         try:
             total = 0
             all = self.db._findAll()
-            with open("export-" + self.getCurrentTime() + ".txt", "w") as file:
+            with open("export-" + getCurrentTime() + ".txt", "w") as file:
                 for domain in all:
                     total = total + len(domain.get('subdomains'))
                     for subdomain in domain.get('subdomains'):
@@ -478,9 +356,9 @@ def banner():
         ██║╚██╔╝██║██║╚██╗██║╚════██║
         ██║ ╚═╝ ██║██║ ╚████║███████║
         ╚═╝     ╚═╝╚═╝  ╚═══╝╚══════╝
-    # {0}
-    # {1}
-    # version {2}
+    ## {0}
+    ## {1}
+    ## version {2}
     """
     print(colored(BANNER.format("Monitor New Subdomain", "@omarelfarsaoui", version), 'red'))
 
