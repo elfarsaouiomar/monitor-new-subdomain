5ea484a32edde4c65b67e71144d9a014f4603304
elfarsaouiomar <omar.farsaoui@outlook.fr>
2023-03-05 12:03:17 +0100

    Merge and rebase

--- src/config.py
+++ src/config.py
@@ -1,15 +1,19 @@
+#slack webhook
 SLACK_WEBHOOK_URL = "https://hooks.slack.com/services/xxxxxxxxxxxxxxxxxx/xxxxxxxxxxxxxxxxxxxxxxxxxx"
 
+# TelegrameBoot
 TELEGRAM_TOKEN = "xxxxxxxxxxxxxxxxxxxxxxxx"
 TELEGRAM_CHAT_ID = "xxxxxxxxxxxxxxxxxxxxxxxxxx"
 
-DB_HOST = "127.0.0.1"
+# MongoDB DB
+DB_HOST = "mongo"
 DB_PORT = 27017
 DB_NAME = "MonitoringSubdomain"
 COLLECTION_NAME = "subdomains"
 DB_USER = ""
 DB_PWD = ""
 
+### list resolver
 RESOLVERS_LIST = [
             '1.1.1.1',
             '1.0.0.1',
