0a9dcd6b0c2ce455009585c4aeff71a2a723cc5c
omar.farsaoui <omar.farsaoui@outlook.fr>
2023-03-16 22:41:31 +0100

    WIP: stil woring on the API

--- API.py
+++ API.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI, HTTPException, BackgroundTasks
 from pymongo import MongoClient
 from src.config import DB_HOST, DB_NAME, COLLECTION_NAME, DB_PORT
+from src.db import db
 from src.mns import SubDomainMonitoring
 
 app = FastAPI()
 mns = SubDomainMonitoring()
+
+
+# TODO Try to use the /app/logs/api.log to save logs
+
+
 
 # Initialize the MongoDB client
 client = MongoClient(f"mongodb://{DB_HOST}:{DB_PORT}")
@@ -64,15 +70,22 @@ async def get_subdomains_by_domain(domain):
 	:return: A list of domain documents.
 	"""
 	subdomains = collection.find_one({"domain": domain})
-	return helper(subdomains)
+	print(subdomains)
+	if subdomains:
+		if len(subdomains) == 0:
+			return response_with_error(message="domain not found", code=404)
+	return response_with_success(message=helper(subdomains), code=200)
 
 
-@app.get("/")
+@app.get("/ping")
 async def index():
 	"""
 	Root endpoint for testing the API.
 	"""
-	return {"status": "ok"}
+	print()
+	for route in app.routes:
+		print(route.path)
+	return response_with_success(message="pong", code=200)
 
 
 async def get_domains():
@@ -81,10 +94,9 @@ async def get_domains():
 
 	Returns A list of domain documents.
 	"""
-	domains = []
-	for doc in collection.find():
-		domains.append(doc.get('domain'))
-	return domains
+
+	domains = [ target.get('domain') for target in collection.find() ]
+	return response_with_success(message=domains)
 
 
 @app.get("/domains")
@@ -93,7 +105,7 @@ async def list_domains():
 		Endpoint for listing all domains in the database.
 	"""
 	domains = await get_domains()
-	return {"domains": domains}
+	return domains
 
 
 @app.get("/domain/{domain}")
@@ -101,10 +113,11 @@ async def get_domain_by_name(domain: str):
 	"""
 		Endpoint for getting subdomains list a domain by name.
 	"""
-	doc = await get_subdomains_by_domain(domain)
-	if not doc:
-		raise HTTPException(status_code=404, detail="Domain not found")
-	return doc
+	response = await get_subdomains_by_domain(domain)
+	return response
+	#if not response:
+		#raise HTTPException(status_code=404, detail="Domain not found")
+
 
 
 async def add_domain(domain: str):
@@ -115,8 +128,10 @@ async def add_domain(domain: str):
 	"""
 	
 	subdomains = await get_subdomains_by_domain(domain)
-	if subdomains is None:
-		mns.add(domain=domain)
+	print(subdomains)
+	print(subdomains.get('subdomains'))
+	if subdomains.get('subdomains') is None:
+		BackgroundTasks(mns.add(domain=domain))
 		return response_with_success(message=domain, code=201)
 	return response_with_error(message="Domain already exists", code=409)
 
@@ -141,10 +156,8 @@ async def delete_domain(domain):
 	"""
 	result = collection.find_one_and_delete({"domain": domain})
 	if result:
-		response = {"message": "Document deleted successfully"}
-	else:
-		response = {"message": "Document not found"}
-	return response
+		return response_with_success(message="domain deleted successfully", code=204)
+	return response_with_error(message="Domain not found", code=404)
 
 
 @app.delete("/domain/{domain}")
@@ -158,12 +171,12 @@ async def delete(domain: str):
 
 async def launch_monitoring():
 	"""
-	This function
-	Returns: string
+	This function used to launch The Monitoring Process
+	Returns: Str -> Start Monitoring
 
 	"""
 	BackgroundTasks(mns.monitor())
-	return "Start Monitoring"
+	return response_with_success(message="start Monitoring")
 
 
 @app.get("/monitor")
