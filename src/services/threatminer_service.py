9e3eadf39579192377ae81f523fa9bc312381b0e
omar.farsaoui <omar.farsaoui@outlook.fr>
2026-01-02 17:40:12 +0100

    Add new version Refactore code Fix bugs improve code quality using AI

---
+++ src/services/threatminer_service.py
@@ -0,0 +1,20 @@
+from requests import get
+
+
+class Threatminer:
+    """
+    get subdomains from https://threatminer/
+
+    """
+
+    def get_subdomains(self, domain):
+        """get subdomains from Threatminer API"""
+
+        url = f"https://api.threatminer.org/v2/domain.php?q={domain}&rt=5"
+        res = get(url=url, timeout=30, verify=True)
+        res.raise_for_status()
+        resp = res.json()
+        if resp.get("results") is not None:
+            return resp.get("results")
+
+        return []
